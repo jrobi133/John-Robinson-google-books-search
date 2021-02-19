02a3bbf0a5dbb4d3a82a1afbd758aff6f9668de6
John Robinson <johnarobinson133@gmail.com>
2021-02-19 10:25:54 -0700

    added pages folder and blank search and saved pages and added them to app.js in src

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Search from './pages/Search';
+import Saved from './pages/Saved';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+      <Route exact path="/" component={Search} />
+      <Route exact path="/search" component={Search} />
+      <Route exact path="/saved" component={Saved} />
+      </Switch>
+      
+    </Router>
+    </>
   );
 }
 
