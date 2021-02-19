21166174590b8721bbc62d1155d75c875757c2ec
John Robinson <johnarobinson133@gmail.com>
2021-02-19 14:09:37 -0700

    added functions needed on search.js page

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -4,7 +4,9 @@ import BookSearch from '../components/BookSearch';
 
 class Search extends Component {
     state = {
-        
+        books: [],
+        savedBooks: [],
+        i: "",
     };
 
     handleInputChange = (event) => {
@@ -13,24 +15,63 @@ class Search extends Component {
     };
 
     getBooks = () => {
-
-    }
+        API.getBooks(this.state.i)
+        .then((res) => {
+            this.setState({
+                books: res.data,
+                currentPage: 1,
+            })
+        }).catch(()=> {
+            this.setState({
+                books: [],
+                message: "Search didnt match any book results",
+                currentPage: 1,
+            });
+        });
+    };
+    getSavedBooks = () => {
+        API.getSavedBooks()
+        .then((res) => {
+            this.setState({ savedBooks: res.data});
+        }).catch (() => {
+            this.setState({
+                savedBooks: [],
+                message: "You didnt save any books"
+            });
+        });
+    };
 
     handleFormSubmit = (event) => {
         event.preventDefault();
         this.getBooks();
     }
-}
 
-function Home () {
-    return (
-        <>
-            <BookSearch 
-            handleFormSubmit={this.handleFormSubmit}
-            handleInputChange={this.handleInputChange}
+    handleBookSave = (id) => {
+        const book = this.state.books.find((book) => book.id === id);
+        const authorsString = book.volumeInfo.authors.toString();
+        API.saveBook({
+            id: book.id,
+            title: book.volumeInfo.title,
+            subtitle: book.volumeInfo.subtitle,
+            link: book.volumeInfo.infoLink,
+            authors: authorsString,
+            description: book.volumeInfo.description,
+            image: book.volumeInfo.imageLinks.thumbnail,
+        }).then(() => this.getSavedBooks());
+    };
+
+    render() {
+        return (
+            <>
+            <BookSearch
+                handleInputChange={this.handleInputChange}
+                handleFormSubmit={this.handleFormSubmit}
+                q={this.state.q}
             />
-        </>
-    )
+            </>
+        )
+    }
 }
 
-export default Home;
+
+export default Search;
