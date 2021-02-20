cf14a8b4cc13ec51f952bb6fda3ff3f27830a2f7
John Robinson <johnarobinson133@gmail.com>
2021-02-19 18:27:58 -0700

    added search controller

---
+++ server/controllers/search.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const { response } = require('express');
+
+module.exports = {
+    findAll: function (req, res) {
+        const params = {
+            i: req.query.i,
+        };
+        const URL = `https://www.googleapis.com/books/v1/volumes?`;
+        axios.get(URL,{params})
+        .then((results) => 
+        results.data.items.filter((results) =>
+        results.volumeInfo.title &&
+        results.volumeInfo.infoLink &&
+        results.volumeInfo.authors &&
+        results.volumeInfo.description &&
+        results.volumeInfo.imageLinks &&
+        results.volumeInfo.imageLinks.thumbnail
+        )).then((response) => {
+            res.json(response);
+        }).catch((err) => res.status(400).json(err));
+    }
+}
