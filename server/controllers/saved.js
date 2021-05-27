10e2c44b01246f007bbdb20908c55c3d81ec6d3a
John Robinson <johnarobinson133@gmail.com>
2021-05-27 11:04:44 -0600

    testing new comp

--- server/controllers/saved.js
+++ server/controllers/saved.js
@@ -10,11 +10,13 @@ module.exports = {
       .then((dbBook) => res.json(dbBook))
       .catch((err) => res.status(422).json(err));
   },
+
   create: function (req, res) {
     db.Book.create(req.body)
       .then((dbBook) => res.json(dbBook))
       .catch((err) => res.status(422).json(err));
   },
+
   remove: function (req, res) {
     db.Book.findById(req.params.id)
       .then((dbBook) => dbBook.remove())
